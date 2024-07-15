20c706cf81cde24356a0eadfc255c04279edb0a6
dandelion08 <dandelion08@163.com>
2024-07-15 18:16:30 +0800

    Update index.js

--- octo-portal-frontend/config/index.js
+++ octo-portal-frontend/config/index.js
@@ -18,7 +18,7 @@ module.exports = {
     },
 
     // Various Dev Server settings
-    host: 'localhost', // can be overwritten by process.env.HOST
+    host: '193.112.70.156', // can be overwritten by process.env.HOST
     port: 8081, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
